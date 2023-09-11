c0f6a2c532053c849ff5e5c19aa93a03c5ed9260
Charles-Antoine Francisco <charlesfranciscodev@gmail.com>
2023-09-11 10:26:17 -0700

    Mars Lander 1 - TypeScript - refactor

--- puzzles/ts/mars-lander1/mars-lander1.ts
+++ puzzles/ts/mars-lander1/mars-lander1.ts
@@ -1,21 +1,32 @@
-const surfaceN: number = parseInt(readline()); // the number of points used to draw the surface of Mars.
-for (let i = 0; i < surfaceN; i++) {
-  readline().split(' ');
-}
-
-// game loop
-while (true) {
-  const inputs: string[] = readline().split(' ');
-  const vSpeed: number = parseInt(inputs[3]); // the vertical speed (in m/s), can be negative.
-  let power: number = parseInt(inputs[6]); // the thrust power (0 to 4).
-
-  let rotationAngle = 0;
-  if (vSpeed <= -40) {
-    power = 4;
-  } else {
-    power = 0;
+function readSurfacePoints(surfaceN: number): void {
+  for (let i = 0; i < surfaceN; i++) {
+    readline().split(' ');
   }
-
-  // 2 integers: rotate power. rotate is the desired rotation angle (should be 0 for level 1), power is the desired thrust power (0 to 4).
-  console.log(`${rotationAngle} ${power}`);
 }
+
+function calculateThrustPower(vSpeed: number): number {
+  if (vSpeed <= -40) {
+    return 4;
+  } else {
+    return 0;
+  }
+}
+
+function gameLoop(): void {
+  while (true) {
+    const inputs: string[] = readline().split(' ');
+    const vSpeed: number = parseInt(inputs[3]); // the vertical speed (in m/s), can be negative.
+
+    const power: number = calculateThrustPower(vSpeed);
+    const rotationAngle = 0;
+
+    // 2 integers: rotate power. rotate is the desired rotation angle (should be 0 for level 1), power is the desired thrust power (0 to 4).
+    console.log(`${rotationAngle} ${power}`);
+  }
+}
+
+const surfaceN: number = parseInt(readline()); // the number of points used to draw the surface of Mars.
+readSurfacePoints(surfaceN);
+
+// Start the game loop
+gameLoop();
