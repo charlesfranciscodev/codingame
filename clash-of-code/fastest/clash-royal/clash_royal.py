8dbb475fdf91433e14f3a327b2fd8b9049b3003a
Charles-A. Francisco <charlesfranciscodev@gmail.com>
2024-02-25 12:30:22 -0800

    Cycle Check, Clash Royal & Sum of Series

--- clash-of-code/fastest/clash-royal/clash_royal.py
+++ clash-of-code/fastest/clash-royal/clash_royal.py
@@ -1,6 +1,7 @@
 def calculate_damage(level):
     return 200 + (level - 1) * 20
 
+
 def clash_royal_match(health, level):
     damage = calculate_damage(level)
     health -= damage
@@ -10,10 +11,12 @@ def clash_royal_match(health, level):
     else:
         return "rawr", health
 
-# Get input
-health = int(input())
-level = int(input())
 
-# Call the function and print the output
-result, remaining_health = clash_royal_match(health, level)
-print(result, remaining_health)
+if __name__ == '__main__':
+    # Get input
+    health = int(input())
+    level = int(input())
+
+    # Call the function and print the output
+    result, remaining_health = clash_royal_match(health, level)
+    print(result, remaining_health)
